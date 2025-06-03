83deab3e8f2aeeec58f5bf2ee8256a75d8461e78
oloco2792 <oloco.github@gmail.com>
2025-06-03 19:59:58 -0400

    Cosas

--- vistas/home.php
+++ vistas/home.php
@@ -1,9 +1,18 @@
-<!--div class="posicion-relativa centrar-vertical">
-    <main>
-        <button><a href="#"></a></button>
-        <button><a href="#"></a></button>
-        <button><a href="#"></a></button>
-        <button><a href="#"></a></button>
+<div class="posicion-relativa centrar-vertical">
+    <main class="contenedor">
+	<?php echo "<h1>Bienvenido, ".$_SESSION['user']."!</h1>";?>
+	<div class="home">
+		<div class="home__deudas caja">
+		<h3>Deudas</h3>
+			<button class="home__boton"><a class="home__enlace" href="#">Visualizar</a></button>
+			<button class="home__boton"><a class="home__enlace" href="#">Registrar</a></button>
+		</div>
+		<div class="home__facturas caja">
+		<h3>Facturas</h3>
+			<button class="home__boton"><a class="home__enlace" href="#">Visualizar</a></button>
+			<button class="home__boton"><a class="home__enlace" href="#">Registrar</a></button>
+		</div>
+	</div>
     </main>
-</div-->
+</div>
 
