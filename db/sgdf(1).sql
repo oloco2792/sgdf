187a9a250eb87ee0061b91afae4e05d3a326dc4b
oloco2792 <oloco.github@gmail.com>
2025-06-05 07:56:53 -0400

    ay pap

---
+++ db/sgdf(1).sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 05-06-2025 a las 11:55:55
+-- Versión del servidor: 8.4.4
+-- Versión de PHP: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sgdf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deudas`
+--
+
+DROP TABLE IF EXISTS `deudas`;
+CREATE TABLE IF NOT EXISTS `deudas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `persona_id` int NOT NULL,
+  `monto` int NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `descripcion` varchar(100) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `fecha_actualizacion` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `descripcion_pago` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `persona_id` (`persona_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `deudas`
+--
+
+INSERT INTO `deudas` (`id`, `persona_id`, `monto`, `fecha`, `estado`, `descripcion`, `fecha_actualizacion`, `descripcion_pago`) VALUES
+(1, 3, 0, '2025-05-31', 'No Pagada', 'aaaaaaaa', '2025-06-01', 'pepe'),
+(2, 3, 0, '2025-06-03', 'No Pagada', 'pago', '2025-06-03', 'pago');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
+--
+
+DROP TABLE IF EXISTS `facturas`;
+CREATE TABLE IF NOT EXISTS `facturas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `proveedor_id` int NOT NULL,
+  `monto` int NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `descripcion` varchar(200) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `fecha_actualizacion` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `descripcion_pago` varchar(200) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `proveedor_id` (`proveedor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `facturas`
+--
+
+INSERT INTO `facturas` (`id`, `proveedor_id`, `monto`, `fecha`, `estado`, `descripcion`, `fecha_actualizacion`, `descripcion_pago`) VALUES
+(1, 1, 123, '2025-05-27', '', 'asdasdads', '', ''),
+(2, 2, 0, '2025-06-03', 'Pagada', 'pago', '2025-06-03', 'pago');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+DROP TABLE IF EXISTS `personas`;
+CREATE TABLE IF NOT EXISTS `personas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `apodo` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `apodo`) VALUES
+(3, 'Oliver', 'Castillo', 'Oloco');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+CREATE TABLE IF NOT EXISTS `proveedores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `razonSocial` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `rif` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `razonSocial`, `rif`) VALUES
+(1, 'Tinito CA', 123456),
+(2, 'ConfiGuayana', 123456789);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(40) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `pass` varchar(40) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `user`, `pass`) VALUES
+(1, 'admin', 'admin1');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `deudas`
+--
+ALTER TABLE `deudas`
+  ADD CONSTRAINT `deudas_ibfk_1` FOREIGN KEY (`persona_id`) REFERENCES `personas` (`id`);
+
+--
+-- Filtros para la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD CONSTRAINT `facturas_ibfk_1` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
